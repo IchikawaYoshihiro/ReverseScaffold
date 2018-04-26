8a4c1702c8631ca59aac476d27b9f3346cb4763c
yosichikaw <yosichikaw@pasonacareer.biz>
2018-04-26 17:36:41 +0900

    fix indent

--- src/traits/ModelGenerator.php
+++ src/traits/ModelGenerator.php
@@ -3,28 +3,28 @@ namespace Ichikawayac\ReverseScaffoldGenerator\Traits;
 
 trait ModelGenerator {
 
-    /**
-     * generate model file
-     */
-    public function generateModel()
-    {
-        $replaces = [
-            'DummyModel'   => $this->ModelName,
-            'DummyColumns' => $this->fillableFields()->pluck('Field')->implode("',\n\t\t'"),
-        ];
+	/**
+	 * generate model file
+	 */
+	public function generateModel()
+	{
+		$replaces = [
+			'DummyModel'   => $this->ModelName,
+			'DummyColumns' => $this->fillableFields()->pluck('Field')->implode("',\n\t\t'"),
+		];
 
-        $stub = static::getStubFile('model.stub');
+		$stub = static::getStubFile('model.stub');
 
-        $this->fileGenerate($stub, $replaces, $this->modelFilePath());
-    }
+		$this->fileGenerate($stub, $replaces, $this->modelFilePath());
+	}
 
-    public function modelFileExists()
-    {
-        return file_exists($this->modelFilePath());
-    }
+	public function modelFileExists()
+	{
+		return file_exists($this->modelFilePath());
+	}
 
-    public function modelFilePath()
-    {
-        return app_path($this->ModelName.'.php');
-    }
+	public function modelFilePath()
+	{
+		return app_path($this->ModelName.'.php');
+	}
 }
